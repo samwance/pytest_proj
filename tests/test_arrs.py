3fada8818f7a532465a2f020fad0c06f459a31d8
semyon <odintsovvans@gmail.com>
2023-06-13 21:03:44 +0600

    achieved 100% test coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,15 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+
+def test_get_out_of_bounds():
+    assert arrs.get([1, 2, 3], 10, "test") == "test"
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
+
+
+def test_my_slice_out_of_bounds():
+    assert arrs.my_slice([1, 2, 3], 0, 10) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], -10, 2) == [1, 2]
+    assert arrs.my_slice([1, 2, 3], -1, 2) == []
+    assert arrs.my_slice([], -10, 2) == []
